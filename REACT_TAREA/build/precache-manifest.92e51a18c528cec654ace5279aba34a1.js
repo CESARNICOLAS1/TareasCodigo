5ca86f01648671e0941dcf96b5986b529a6513c8
CESARNICOLAS1 <CENICOAR@GMAIL.COM>
2020-06-09 16:43:18 -0500

    TAREA REACT

---
+++ REACT_TAREA/build/precache-manifest.92e51a18c528cec654ace5279aba34a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c82064124457b79106ad48827dd4ccd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a960ec505a026d4daa00",
+    "url": "/static/js/2.5455fb9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5455fb9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3011290acf1c21e44f46",
+    "url": "/static/js/main.30e07ae4.chunk.js"
+  },
+  {
+    "revision": "ef03fad86a919ef26f97",
+    "url": "/static/js/runtime-main.3ada473c.js"
+  }
+]);
